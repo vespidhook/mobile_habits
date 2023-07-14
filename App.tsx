b4fce4751f08fb53333aa324f47f6d2585e403e9
Bruno Alves <vespidhook@gmail.com>
2023-07-14 18:42:54 -0300

    iniciando

--- App.tsx
+++ App.tsx
@@ -1,33 +1,20 @@
-import { StatusBar, View } from 'react-native';
-import { 
-  useFonts, 
-  Inter_400Regular,
-  Inter_600SemiBold,
-  Inter_700Bold,
-  Inter_800ExtraBold
-} from '@expo-google-fonts/inter';
-
-import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_600SemiBold,
-    Inter_700Bold,
-    Inter_800ExtraBold,
-  });
-
-  if (!fontsLoaded) {
-    return (
-      <Loading />
-    )
-  }
-
   return (
-    <View>
-      <Home />
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/screens/Home.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
-}
