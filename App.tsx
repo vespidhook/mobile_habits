7e4f3be1cb64a6f13adcbab64254dddff6cb397a
Bruno Alves <vespidhook@gmail.com>
2023-07-14 18:19:59 -0300

    configs

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import { 
   useFonts, 
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
-  Inter_800ExtraBold,
+  Inter_800ExtraBold
 } from '@expo-google-fonts/inter';
+
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,22 +25,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090a',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'red',
-    fontFamily: 'Inter_800ExtraBold',
-  }
-});
