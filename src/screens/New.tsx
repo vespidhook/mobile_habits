6c1df6ad8c5e9c121ad78c0b1a5792193b1b51b9
Bruno Alves <vespidhook@gmail.com>
2023-07-20 16:39:26 -0300

    new habit api

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useState } from "react";
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
@@ -16,6 +16,7 @@ const availableWeekDays = [
 ];
 
 export function New() {
+  const [title, setTitle] = useState('')
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
   function handleToggleWeekDay(weekDayIndex: number) {
@@ -23,6 +24,19 @@ export function New() {
       setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex))
     } else {
       setWeekDays(prevState => [...prevState, weekDayIndex]) ;
+    }
+  }
+
+  async function handleCreateNewHabit() {
+    try {
+      if(!title.trim() || weekDays.length === 0) {
+        Alert.alert('Novo Hábito', 'Informe o nome do hábito e escolha a periodicidade')
+      }
+
+      await api.post('')
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Ops', 'Não foi possivel criar o novo hábito')
     }
   }
 
@@ -47,6 +61,8 @@ export function New() {
         className="h-12 pl-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
         placeholder=" Exercícios, dormir bem, etc..."
         placeholderTextColor={colors.zinc[400]}
+        onChangeText={setTitle}
+        value={title}
         />
 
         <Text className="font-semibold mt-4 mb-3 text-white text-base">
@@ -67,6 +83,7 @@ export function New() {
         <TouchableOpacity
           className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
           activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
         >
           <Feather 
             name="check"
